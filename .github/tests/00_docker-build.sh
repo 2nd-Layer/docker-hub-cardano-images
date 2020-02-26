0015762958b0f8d1757242fd855688a31962d55c
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-26 14:25:29 +0100

    Update actions

--- .github/tests/00_docker-build.sh
+++ .github/tests/00_docker-build.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -e
 
-echo ${1}
+if [ -z ${1+x} ] || [ ${1} == 'master' ]; then
+  for Dockerfile in $(find -name Dockerfile); do
+    imageName=$(echo ${Dockerfile} | awk -F '/' '{ print $2 }')
+    imageVersion=$(echo ${Dockerfile} | awk -F '/' '{ print $3 }')
+    imageTag=${repositoryName}/${imageName}:${imageVersion}
+    dockerfileDir=${imageName}/${imageVersion}
+    fnBuildDockerImage
+  done
+else
+  imageName=$(echo ${1} | awk -F '-' '{ print $2 }')
+  imageVersion=$(echo ${1} | awk -F '-' '{ print $3 }')
+  imageTag=${repositoryName}/${imageName}:${imageVersion}
+  dockerfileDir=${imageName}/${imageVersion}
+fi
 
-repositoryName="2ndlayer"
-
-for Dockerfile in $(find -name Dockerfile); do
-  imageName=$(echo ${Dockerfile} | awk -F '/' '{ print $2 }')
-  imageVersion=$(echo ${Dockerfile} | awk -F '/' '{ print $3 }')
+function fnBuildDockerImage {
   imageTag=${repositoryName}/${imageName}:${imageVersion}
   dockerfileDir=${imageName}/${imageVersion}
   echo "Building Dockerfile for ${imageTag}"
   pushd ${dockerfileDir}
-  docker build -t ${imageTag} ./
+    docker build -t ${imageTag} ./
   popd
-done
+}
