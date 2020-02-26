c5b1cc85300c942c90e70fad894030bc51745962
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-26 15:13:49 +0100

    Update actions

--- .github/tests/00_docker-build.sh
+++ .github/tests/00_docker-build.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ $1 == 'merge' ]; then
-  branch=${2}
+  branchName=${2}
 fi
 
-echo "Running on branch: ${1}"
+echo "Running on branch: ${branchName}"
 
 function fnBuildDockerImage {
   imageTag=${repositoryName}/${imageName}:${imageVersion}
@@ -21,8 +21,8 @@ repositoryName='2ndlayer'
 if [ -z ${1+x} ]; then
   echo "Not enough arguments provided!"
   exit 1
-elif [ ${branch} == 'master' ]; then
-  echo "Running on branch: ${branch}; building all images."
+elif [ ${branchName} == 'master' ]; then
+  echo "Running on branch: ${branchName}; building all images."
   for Dockerfile in $(find -name Dockerfile); do
     imageName=$(echo ${Dockerfile} | awk -F '/' '{ print $2 }')
     imageVersion=$(echo ${Dockerfile} | awk -F '/' '{ print $3 }')
@@ -30,9 +30,9 @@ elif [ ${branch} == 'master' ]; then
     dockerfileDir=${imageName}/${imageVersion}
     fnBuildDockerImage
   done
-elif [[ ${branch} =~ ^(add|update)-(jormungandr|cardano-node)-[0-9]+.*$ ]]; then
-  imageName=$(echo ${branch} | awk -F '-' '{ print $2 }')
-  imageVersion=$(echo ${branch} | awk -F '-' '{ print $3 }')
+elif [[ ${branchName} =~ ^(add|update)-(jormungandr|cardano-node)-[0-9]+.*$ ]]; then
+  imageName=$(echo ${branchName} | awk -F '-' '{ print $2 }')
+  imageVersion=$(echo ${branchName} | awk -F '-' '{ print $3 }')
   imageTag=${repositoryName}/${imageName}:${imageVersion}
   dockerfileDir=${imageName}/${imageVersion}
   fnBuildDockerImage
