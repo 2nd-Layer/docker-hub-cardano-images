3ab079daa5008d9c24b38dddd63a8f1a048d1cce
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-26 14:27:30 +0100

    Update actions

--- .github/tests/00_docker-build.sh
+++ .github/tests/00_docker-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "Running on branch: ${1}"
+
 if [ -z ${1+x} ] || [ ${1} == 'master' ]; then
   for Dockerfile in $(find -name Dockerfile); do
     imageName=$(echo ${Dockerfile} | awk -F '/' '{ print $2 }')
