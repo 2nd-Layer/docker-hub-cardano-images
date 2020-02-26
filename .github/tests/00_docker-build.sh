f91b6b9b00dc7ea1f2ecc452dc57e855d1669239
Mark Stopka <mark.stopka@perlur.cloud>
2020-02-26 13:14:17 +0100

    Add GitHub Actions

---
+++ .github/tests/00_docker-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+repositoryName="2ndlayer"
+
+for Dockerfile in $(find -name Dockerfile); do
+  imageName=$(echo ${Dockerfile} | awk -F '/' '{ print $2 }')
+  imageVersion=$(echo ${Dockerfile} | awk -F '/' '{ print $3 }')
+  imageTag=${repositoryName}/${imageName}:${imageVersion}
+  dockerfileDir=${imageName}/${imageVersion}
+  echo "Building Dockerfile for ${imageTag}"
+  pushd ${dockerfileDir}
+  docker build -t ${imageTag} ./
+  popd
+done
