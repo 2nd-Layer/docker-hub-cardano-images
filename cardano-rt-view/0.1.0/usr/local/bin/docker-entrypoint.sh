daa83bb4927ebf2a183543edfd1001fd8d02bae8
Mark Stopka <mark.stopka@notino.com>
2020-10-23 07:29:59 +0200

    Docker entrypoint improvements

--- cardano-rt-view/0.1.0/usr/local/bin/docker-entrypoint.sh
+++ cardano-rt-view/0.1.0/usr/local/bin/docker-entrypoint.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-#source /usr/local/lib/cardano-node-set-env-variables.sh
+source /usr/local/lib/cardano-rt-view-set-env-variables.sh
 
 function preExitHook () {
   exec "$@"
   echo 'Exiting...'
 }
 
+if [[ ! -f ${CNODE_RT_VIEW_CONF_FILE} ]]; then
+  echo "'cardano-rt-view' config file does not exists! 'cardano-rt-view' can NOT start!!!"
   preExitHook "$@"
   exit
+fi
